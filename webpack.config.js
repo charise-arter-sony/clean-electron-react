76504bfd411a16c773b3bf85761d48d709d22ad1
Reese <arter.charise@gmail.com>
2022-10-25 19:01:04 -0700

    initial setup of webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	// Electron entry point
+	entry: './app/src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'app/dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			// loads .js/jsx/json files
+			{
+				test: /\.jsx?$/,
+				include: [path.resolve(__dirname, 'app/src')],
+				loader: 'babel-loader',
+				resolve: {
+					extensions: ['.js', '.jsx', '.json'],
+				},
+			},
+		],
+	},
+};
