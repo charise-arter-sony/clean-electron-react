262294d6521e5d052c06da305941a94571e53e2e
charise-arter-sony <charise.arter@sony.com>
2022-11-03 18:27:37 -0700

    reattempting counter ipc with redux

---
+++ app/electron/state/main_slices/counterSlice.js
@@ -0,0 +1,1 @@
+const { } = require('@reduxjs/toolkit')
--- app/src/components/Counter.jsx
+++ app/src/components/Counter.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function Counter({ count }) {
-  return (
-    <section>
-      <h2>Counter</h2>
+	return (
+		<section>
+			<h2>Counter - Main to Renderer</h2>
+			<p> Main does logic, while Renderer has state</p>
 
-      <h3 id="counterValue">{count}</h3>
-      <button id="btnIncrement"> Increment +</button>
-      <button id="btnDecrement"> Decrement -</button>
-
-    </section>
-  )
+			<h3 id='counterValue'>{count}</h3>
+		</section>
+	);
 }
 
-export default Counter
+export default Counter;
